1401b56a6e20a1de1013b073c269f2fa565fa945
DevDCruz <juandecruzsan@gmail.com>
2024-04-05 17:56:48 +0200

    creation of DetailActivity and conect with navigation

---
+++ app/src/main/java/com/devdcruz/mymovies/DetailActivity.kt
@@ -0,0 +1,21 @@
+package com.devdcruz.mymovies
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devdcruz.mymovies.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_TITLE = "DetailActivity:title"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val title = intent.getStringExtra(EXTRA_TITLE)
+        binding.tvDetailTitle.text = title
+        binding
+    }
+}
--- app/src/main/java/com/devdcruz/mymovies/MainActivity.kt
+++ app/src/main/java/com/devdcruz/mymovies/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.devdcruz.mymovies
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
 import androidx.lifecycle.lifecycleScope
 import com.devdcruz.mymovies.databinding.ActivityMainBinding
+import com.devdcruz.mymovies.model.Movie
 import com.devdcruz.mymovies.model.MovieDbClient
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -19,7 +21,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val moviesAdapter = MoviesAdapter(emptyList()) { movie ->
-            Toast.makeText(this@MainActivity, movie.title, Toast.LENGTH_SHORT).show()
+            navigateTo(movie)
         }
         binding.recyclerMovie.adapter = moviesAdapter
         lifecycleScope.launch {
@@ -28,5 +30,11 @@ class MainActivity : AppCompatActivity() {
             moviesAdapter.movies = popularMovies.results
             moviesAdapter.notifyDataSetChanged()
         }
+    }
+
+    private fun navigateTo(movie: Movie) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.putExtra(DetailActivity.EXTRA_TITLE, movie.title)
+        startActivity(intent)
     }
 }
