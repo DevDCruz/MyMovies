d838675227575c52129945004df715580b2aab5d
DevDCruz <juandecruzsan@gmail.com>
2024-04-07 11:53:10 +0200

    conection detail activity with intent and correction of parcelable

--- app/src/main/java/com/devdcruz/mymovies/DetailActivity.kt
+++ app/src/main/java/com/devdcruz/mymovies/DetailActivity.kt
@@ -1,21 +1,40 @@
 package com.devdcruz.mymovies
 
+import android.content.Intent
+import android.os.Build.VERSION.SDK_INT
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Parcelable
+import com.bumptech.glide.Glide
 import com.devdcruz.mymovies.databinding.ActivityDetailBinding
+import com.devdcruz.mymovies.model.Movie
 
 class DetailActivity : AppCompatActivity() {
 
     companion object {
-        const val EXTRA_TITLE = "DetailActivity:title"
+        const val EXTRA_MOVIE = "DetailActivity:movie"
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val title = intent.getStringExtra(EXTRA_TITLE)
-        binding.tvDetailTitle.text = title
-        binding
+
+        val movie = intent.parcelable<Movie>(EXTRA_MOVIE)
+        if (movie != null) {
+            binding.tvDetailTitle.text = movie.title
+            Glide
+                .with(this)
+                .load("https://image.tmdb.org/t/p/w780${movie.backdrop_path}")
+                .into(binding.ivDetailBackdrop)
+        }
+
     }
+
+    private inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? =
+        when {
+            SDK_INT >= 33 -> getParcelableExtra(key, T::class.java)
+            else -> @Suppress("DEPRECATION") getParcelableExtra(key) as? T
+        }
 }
