04947e6a482306ae0175fa074cedfa175b2db11f
DevDCruz <juandecruzsan@gmail.com>
2024-04-08 11:37:57 +0200

    DetailinfoActivity completed

--- app/src/main/java/com/devdcruz/mymovies/DetailActivity.kt
+++ app/src/main/java/com/devdcruz/mymovies/DetailActivity.kt
@@ -5,6 +5,9 @@ import android.os.Build.VERSION.SDK_INT
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.widget.TextView
+import androidx.core.text.bold
+import androidx.core.text.buildSpannedString
 import com.bumptech.glide.Glide
 import com.devdcruz.mymovies.databinding.ActivityDetailBinding
 import com.devdcruz.mymovies.model.Movie
@@ -23,13 +26,30 @@ class DetailActivity : AppCompatActivity() {
 
         val movie = intent.parcelable<Movie>(EXTRA_MOVIE)
         if (movie != null) {
-            binding.tvDetailTitle.text = movie.title
+            title = movie.title
             Glide
                 .with(this)
                 .load("https://image.tmdb.org/t/p/w780${movie.backdrop_path}")
                 .into(binding.ivDetailBackdrop)
+            binding.tvDetailSummary.text = movie.overview
+            bindDetailInfo(binding.tvDetailInfo, movie)
         }
 
+    }
+
+    private fun bindDetailInfo(tvDetailInfo: TextView, movie: Movie) {
+        tvDetailInfo.text = buildSpannedString {
+            bold { append("Original language: ") }
+            appendLine(movie.original_language)
+            bold { append("Original title: ") }
+            appendLine(movie.original_title)
+            bold { append("Release date: ") }
+            appendLine(movie.release_date)
+            bold { append("Popularity: ") }
+            appendLine(movie.popularity.toString())
+            bold { append("Vote average: ") }
+            appendLine(movie.vote_average.toString())
+        }
     }
 
     private inline fun <reified T : Parcelable> Intent.parcelable(key: String): T? =
